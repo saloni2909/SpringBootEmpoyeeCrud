423378080b16fea4c34014359595ced3b2792e12
AnujKrSharma <anujkrsharma@hotmail.com>
2024-11-16 20:31:40 +0530

    inital Commit

---
+++ src/main/java/com/saluj/springbootemployeecrud/SpringBootEmployeeCrudApplication.java
@@ -0,0 +1,13 @@
+package com.saluj.springbootemployeecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmployeeCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEmployeeCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saluj/springbootemployeecrud/SpringBootEmployeeCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saluj.springbootemployeecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmployeeCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
